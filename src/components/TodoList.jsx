cef8d1a4160d85f41e49767f577cfceb4639bbac
parkyejin <uiop5809@naver.com>
2022-11-03 01:39:26 +0900

    selector

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
-import { todoListState } from "./TodoStore.js";
+import React from "react";
+import { filteredTodoListState } from "./TodoStore.js";
 import { useRecoilValue } from "recoil";
 import TodoItem from "./TodoItem.jsx";
 import TodoListCreator from "./TodoListCreator.jsx";
+import TodoListFilters from "./TodoListFilters.jsx";
+import TodoListStats from "./TodoListStats.jsx";
 
 export default function TodoList() {
-  const todoList = useRecoilValue(todoListState);
-
-  useEffect(() => {
-    console.log(`todoList: ${JSON.stringify(todoList)}`);
-  }, [todoList]);
+  const todoList = useRecoilValue(filteredTodoListState);
 
   return (
     <div>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoListCreator />
       {todoList.map((item) => (
         <TodoItem key={item.id} item={item} />
